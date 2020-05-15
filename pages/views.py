04e29d533af2b8d22dfb6671df017e720bbfda1f
Leo Kukharau <kuhorev.leonid@gmail.com>
2020-05-15 16:09:32 -0700

    search values are saved after form submitted

--- pages/views.py
+++ pages/views.py
@@ -1,12 +1,19 @@
 from django.shortcuts import render
 
+from listings.choices import bedroom_choices, price_choices, state_choices
 from listings.models import Listing
 from realtors.models import Realtor
 
 
 def index(request):
     listings = Listing.objects.order_by("-list_date").filter(is_published=True)[:3]
-    context = {"listings": listings}
+
+    context = {
+        "listings": listings,
+        "bedroom_choices": bedroom_choices,
+        "price_choices": price_choices,
+        "state_choices": state_choices,
+    }
     return render(request, "pages/index.html", context)
 
 
